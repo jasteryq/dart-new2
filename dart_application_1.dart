e85c8ae42b82534617b417be3d136da2fd1050fb
CHVK Redan <129118860+jasteryq@users.noreply.github.com>
2023-04-12 14:02:58 +0300

    Add files via upload

---
+++ dart_application_1.dart
@@ -0,0 +1,16 @@
+void main () {
+String name = "shrek";
+print(name);
+name = "Osel";
+print(name);
+var x = "a+b";
+print(x);
+dynamic y = "a-b";
+print(y);
+y = "45-7";
+print(y);
+const p = "MMM";
+print(p);
+final todey = DateTime.now();
+print(todey);
+}
