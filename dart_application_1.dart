3542e06d74b3948a3a5b0280e08dda0efffbb56e
CHVK Redan <129118860+jasteryq@users.noreply.github.com>
2023-04-12 14:37:35 +0300

    primitive data types

--- dart_application_1.dart
+++ dart_application_1.dart
@@ -1,16 +1,28 @@
-void main () {
-String name = "shrek";
+void main(){
+bool x = true;
+bool y = false;
+int name = -15;
 print(name);
-name = "Osel";
-print(name);
-var x = "a+b";
-print(x);
-dynamic y = "a-b";
-print(y);
-y = "45-7";
-print(y);
-const p = "MMM";
-print(p);
-final todey = DateTime.now();
-print(todey);
+var g = -15.5;
+print(g);
+double v = 8.8;
+print(v);
+String b = "naruto";
+print(b);
+String text = '''
+fff
+fff
+''';
+print(text);
+String text2 = """
+И ты раскаешься, бедный брат.
+Заблудший брат, ты будешь прощён.
+Под песнопения в свой квадрат
+ты будешь бережно возвращён.
+""";
+print(text2);
+String name2  = "Tomas";
+String age = "25cm";
+String info = "name2: $name2  $age";
+print(info);
 }
